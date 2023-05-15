d29201c821b56fd7743b8608d3c16f4bac0f8836
Ignat <romign@gmail.com>
2023-05-15 16:13:40 +0100

    feat: add vercel analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,14 @@ import "@/app/globals.css";
 import CardLayout from "@/components/layouts/CardLayout";
 import { AppProps } from "next/app";
 import { WalletProvider } from "@/contexts/WalletContext";
+import { Analytics } from "@vercel/analytics/react";
 
 const RPSLSApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
     <WalletProvider>
       <CardLayout>
         <Component {...pageProps} />
+        <Analytics />
       </CardLayout>
     </WalletProvider>
   );
